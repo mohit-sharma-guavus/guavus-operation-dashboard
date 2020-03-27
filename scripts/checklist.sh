50d9873c8dd91dcd6a3e6a4938be79c0e4fda596
shivam gupta <shivam.gupta@guavus.com>
2020-03-27 11:08:17 +0530

    At same level to latest code

--- scripts/checklist.sh
+++ scripts/checklist.sh
@@ -26,99 +26,109 @@ UPTIME=`cat /proc/uptime | awk '{print $1}'`
 UPTIME_INT=`echo ${UPTIME%.*}`
 FILE_VALUE=50
 IP_FORWARD_VALUE=`cat /proc/sys/net/ipv4/ip_forward`
+date_time=`date "+%Y-%m-%d_%H:%M"`
 
-function green_echo() {
-  echo -e "\x1b[1;32m$1\e[0m"
-}
+#function green_echo() {
+#  echo -e "\x1b[1;32m$1\e[0m"
+#}
 
-function red_echo() {
-  echo -e "\x1b[1;31m$1\e[0m"
-}
+#function red_echo() {
+#  echo -e "\x1b[1;31m$1\e[0m"
+#}
 
-
-echo "###############################################" > /tmp/Checklist_"$HNAME"_"$day".txt
-echo "#################  $HNAME  ####################" >> /tmp/Checklist_"$HNAME"_"$day".txt
-echo "###############################################" >> /tmp/Checklist_"$HNAME"_"$day".txt
+> /tmp/Checklist_PASSED.txt
+> /tmp/Checklist_FAILED.txt
 
 if [  $UPTIME_INT -gt 21600  ]
 then
-   green_echo "PASSED - Uptime is more than 6hrs" >> /tmp/Checklist_"$HNAME"_"$day".txt
+   echo "PASSED - Uptime > 6hrs" >> /tmp/Checklist_PASSED.txt
 else
-   red_echo "FAILED - Uptime is less than 6hrs" >> /tmp/Checklist_"$HNAME"_"$day".txt
+   echo "FAILED - Uptime < 6hrs" >> /tmp/Checklist_FAILED.txt
 fi
 
 if [ $OS == $VERSION ]
 then
-	green_echo "PASSED - Linux-OSversion: ${OS} " >>  /tmp/Checklist_"$HNAME"_"$day".txt
+	echo "PASSED -  ${OS} " >>  /tmp/Checklist_PASSED.txt
 else
-	red_echo "FAILED - Linux-OSversion:${OS} " >> /tmp/Checklist_"$HNAME"_"$day".txt
+	echo "FAILED -  ${OS} " >> /tmp/Checklist_FAILED.txt
 fi
 
-green_echo "PASSED - Kernal-version: ${KV} " >> /tmp/Checklist_"$HNAME"_"$day".txt
+echo "PASSED -  ${KV} " >> /tmp/Checklist_PASSED.txt
 
 if [ -z "$OPT_SIZE" ]
 then
-  red_echo "FAILED - /opt/repos Filesystem NOT FOUND"  >> /tmp/Checklist_"$HNAME"_"$day".txt
+  echo "FAILED - /opt/repos FS NOT FOUND"  >> /tmp/Checklist_FAILED.txt
 else
   if [ `echo $OPT_SIZE` -gt 400 ]
   then
-	 green_echo "PASSED - /opt/repos Filesystem size:" ${OPT_SIZE} >> /tmp/Checklist_"$HNAME"_"$day".txt
+	  echo "PASSED - /opt/repos FS size:" ${OPT_SIZE} >> /tmp/Checklist_PASSED.txt
   else
-	red_echo "FAILED - /opt/repos Filesystem size:" ${OPT_SIZE} >> /tmp/Checklist_"$HNAME"_"$day".txt
+	  echo "FAILED - /opt/repos FS size:" ${OPT_SIZE} >> /tmp/Checklist_FAILED.txt
   fi
 fi
 
 if [ $VAR_SIZE -ge $FILE_VALUE  ]
 then
-        green_echo "PASSED - /var/log Filesystem size: ${VAR_SIZE} " >> /tmp/Checklist_"$HNAME"_"$day".txt
+      echo "PASSED - /var/log FS size: ${VAR_SIZE} " >> /tmp/Checklist_PASSED.txt
 else
-        red_echo "FAILED - /var/log Filesystem size: ${VAR_SIZE} " >> /tmp/Checklist_"$HNAME"_"$day".txt
+      echo "FAILED - /var/log FS size: ${VAR_SIZE} " >> /tmp/Checklist_FAILED.txt
 fi
 
 if [ $ROOT_SIZE -ge $FILE_VALUE  ]
 then
-         green_echo "PASSED - / Filesystem size: ${ROOT_SIZE} " >> /tmp/Checklist_"$HNAME"_"$day".txt
+      echo "PASSED - / FS size: ${ROOT_SIZE} " >> /tmp/Checklist_PASSED.txt
 else
-        red_echo "FAILED - / Filesystem size: ${ROOT_SIZE} " >> /tmp/Checklist_"$HNAME"_"$day".txt
+      echo "FAILED - / FS size: ${ROOT_SIZE} " >> /tmp/Checklist_FAILED.txt
 fi
 
-green_echo "CPU : ${CPU} "  >> /tmp/Checklist_"$HNAME"_"$day".txt
-green_echo "MEMORY in GB : ${MEM} " >> /tmp/Checklist_"$HNAME"_"$day".txt
-green_echo "Interface Link MTU is : ${MTU} "  >> /tmp/Checklist_"$HNAME"_"$day".txt
+echo "CPU : ${CPU} "  >> /tmp/Checklist_PASSED.txt
+echo "MEMORY : ${MEM} " >> /tmp/Checklist_PASSED.txt
+echo "Interface MTU is : ${MTU} "  >> /tmp/Checklist_PASSED.txt
 
 if [  $SWAP -eq 0 ]
 then
-    green_echo "PASSED - SWAP is off" >> /tmp/Checklist_"$HNAME"_"$day".txt
+    echo "PASSED - SWAP off" >> /tmp/Checklist_PASSED.txt
 else
-    red_echo "FAILED - SWAP is Available" >> /tmp/Checklist_"$HNAME"_"$day".txt
+    echo "FAILED - SWAP on" >> /tmp/Checklist_FAILED.txt
 fi
 
 if [  $SE_STATUS == "Disabled" -o  $SE_STATUS == "Permissive" ]
 then
-	green_echo "PASSED - Selinux off" >> /tmp/Checklist_"$HNAME"_"$day".txt
+	echo "PASSED - Selinux Disabled" >> /tmp/Checklist_PASSED.txt
 else
-    red_echo "FAILED - Selinux is Enabled " >> /tmp/Checklist_"$HNAME"_"$day".txt
+  echo "FAILED - Selinux Enabled " >> /tmp/Checklist_FAILED.txt
 fi
 
 systemctl status firewalld.service
 if [ $? -eq 0 ]
 then
-	red_echo "FAILED - Firewalld service is Active" >> /tmp/Checklist_"$HNAME"_"$day".txt
+	echo "FAILED - Firewalld Active" >> /tmp/Checklist_FAILED.txt
 else
-    green_echo "PASSED - Firewalld service Stoped" >> /tmp/Checklist_"$HNAME"_"$day".txt
+  echo "PASSED - Firewalld Stopped" >> /tmp/Checklist_PASSED.txt
 fi
 
 systemctl status ntpd.service
 if [ $? -eq 0 ]
 then
-     green_echo "PASSED - NTPD service is Configured" >> /tmp/Checklist_"$HNAME"_"$day".txt
+  echo "PASSED - NTPD Configured" >> /tmp/Checklist_PASSED.txt
 else
-     red_echo "FAILED - NTPD is not Configured" >> /tmp/Checklist_"$HNAME"_"$day".txt
+  echo "FAILED - NTPD not Configured" >> /tmp/Checklist_FAILED.txt
 fi
 
 if [ $IP_FORWARD_VALUE -eq 1 ]
 then
-   green_echo "PASSED - IP Forwarding is Enabled" >> /tmp/Checklist_"$HNAME"_"$day".txt
+  echo "PASSED - IP Forwarding Enabled" >> /tmp/Checklist_PASSED.txt
 else
-   red_echo "FAILED - IP Forwarding is Disable" >> /tmp/Checklist_"$HNAME"_"$day".txt
+  echo "FAILED - IP Forwarding Disable" >> /tmp/Checklist_FAILED.txt
 fi
+
+
+echo "######################################" > /tmp/Checklist_"$HNAME"_"$day".txt
+echo "$HNAME" >> /tmp/Checklist_"$HNAME"_"$day".txt
+echo "######################################" >> /tmp/Checklist_"$HNAME"_"$day".txt
+echo "$date_time" >> /tmp/Checklist_"$HNAME"_"$day".txt
+echo "########  PASSED CHECKS   ############" >> /tmp/Checklist_"$HNAME"_"$day".txt
+cat /tmp/Checklist_PASSED.txt >> /tmp/Checklist_"$HNAME"_"$day".txt
+echo "######### FAILED CHECKS   ############" >> /tmp/Checklist_"$HNAME"_"$day".txt
+cat /tmp/Checklist_FAILED.txt >> /tmp/Checklist_"$HNAME"_"$day".txt
+echo "#######################################" >> /tmp/Checklist_"$HNAME"_"$day".txt
--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,7 @@
 day=`date +%d%m%y`
 SSH_USERNAME=guavus
 PASSWORD="guavus@123"  # Put Root Password Here
-
+REPORTS="/Users/mohitsharma/Desktop/operaton-dashboard/scripts/reports"
 #There must be entry of all fqdn in knowhost file.
 
 which sshpass
@@ -20,15 +20,23 @@ rm -rf /Users/mohitsharma/Desktop/operaton-dashboard/scripts/reports/Ch*
 for i in `cat /Users/mohitsharma/Desktop/operaton-dashboard/scripts/hosts`
 do
 echo $i
-/usr/local/bin/sshpass -p "$PASSWORD" scp -o StrictHostKeyChecking=no /Users/mohitsharma/Desktop/operaton-dashboard/scripts/checklist.sh "$SSH_USERNAME"@"$i":/tmp/.
-/usr/local/bin/sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$SSH_USERNAME"@"$i" "chmod +x /tmp/checklist.sh"
-/usr/local/bin/sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$SSH_USERNAME"@"$i" "sh /tmp/checklist.sh"
+/usr/local/bin/sshpass -p "$PASSWORD" scp -o StrictHostKeyChecking=no /Users/mohitsharma/Desktop/operaton-dashboard/scripts/checklist_new.sh "$SSH_USERNAME"@"$i":/tmp/.
+/usr/local/bin/sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$SSH_USERNAME"@"$i" "chmod +x /tmp/checklist_new.sh"
+/usr/local/bin/sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$SSH_USERNAME"@"$i" "sh /tmp/checklist_new.sh"
 /usr/local/bin/sshpass -p "$PASSWORD" scp -o StrictHostKeyChecking=no "$SSH_USERNAME"@"$i":/tmp/Checklist_"$i"_"$day".txt /Users/mohitsharma/Desktop/operaton-dashboard/scripts/reports/.
 done
 
->/Users/mohitsharma/Desktop/operaton-dashboard/scripts/Checklist_All_Host.txt
+rm /Users/mohitsharma/Desktop/operaton-dashboard/scripts/Checklist_All_Host.txt
 
 for j in `cat /Users/mohitsharma/Desktop/operaton-dashboard/scripts/hosts`
 do
 cat /Users/mohitsharma/Desktop/operaton-dashboard/scripts/reports/Checklist_"$j"_"$day".txt >> /Users/mohitsharma/Desktop/operaton-dashboard/scripts/Checklist_All_Host.txt
+done
+
+rm /Users/mohitsharma/Desktop/operaton-dashboard/scripts/reports/tabular_report.csv
+
+
+for h in `ls -l "$REPORTS" | awk '{ print $9 }'`
+do
+awk 'ORS=NR%16?",":"\n"' "$REPORTS"/"$h" >> "$REPORTS"/tabular_report.csv
 done
